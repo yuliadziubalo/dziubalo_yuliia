11fcd7b18f9c1f44463f37b8efec972d364d2e38
Yuliia Dziubalo <yulia.dzubalo@gmail.com>
2021-11-29 11:05:49 +0200

    Переклад тестового виводу - вивід тексту українською

--- Source.cpp
+++ Source.cpp
@@ -8,18 +8,15 @@ using namespace std;
 int main() 
 {
 	double realiz;
-	cout << "Count of realizations: ";
+	cout << "К-сть реалізацій: ";
 	cin >> realiz;
 
 	double x1 =0.1 , x2 = 0.3, x3 = 0.55, x4 = 0.05;
 	double counter[4] = { 0 };
 	double V[4] = { 0 };
 
-	/*counter1 = gener(realiz, P, counter1);
-	cout << "\nResult 1: " << counter1 << endl;*/
-
 	generation(realiz, x1, x2, x3, x4, counter, V);
-	cout << "\nResult: " << counter[0] << " " << counter[1] << " "
+	cout << "\nРезультат: " << counter[0] << " " << counter[1] << " "
 		<< counter[2]<< " " << counter[3] << endl;
 	cout << "V: " << V[0] << " "<< V[1] << " " << V[2] << " "<< V[3] << endl;
 	double sum = 0;
@@ -27,7 +24,7 @@ int main()
 	{
 		sum += counter[i];
 	}
-	cout << "Sum: " << sum;
+	cout << "Сума: " << sum;
 	
 	return 0;
 
