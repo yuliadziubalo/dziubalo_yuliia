4d97be526c59e357c4f2b60b49cb8ca90289f73a
Yuliia Dziubalo <yulia.dzubalo@gmail.com>
2021-11-29 11:44:58 +0200

    adding some code

--- Source.cpp
+++ Source.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int main() 
 {
 	double realiz;
-	cout << "Count of realizations: ";
+	cout << "К-сть реалізацій: ";
 	cin >> realiz;
 
 	double x1 =0.1 , x2 = 0.3, x3 = 0.55, x4 = 0.05;
@@ -19,15 +19,20 @@ int main()
 	cout << "\nResult 1: " << counter1 << endl;*/
 
 	generation(realiz, x1, x2, x3, x4, counter, V);
-	cout << "\nResult: " << counter[0] << " " << counter[1] << " "
+	cout << "\nРезультат: " << counter[0] << " " << counter[1] << " "
 		<< counter[2]<< " " << counter[3] << endl;
-	cout << "V: " << V[0] << " "<< V[1] << " " << V[2] << " "<< V[3] << endl;
+	cout << "V: ";
+	for (int i = 0; i < 4; i++)
+	{
+		cout << V[i] << " ";
+	}
+
 	double sum = 0;
 	for (int i = 0; i < 4; i++)
 	{
 		sum += counter[i];
 	}
-	cout << "Sum: " << sum;
+	cout << "Сума: " << sum;
 	
 	return 0;
 
